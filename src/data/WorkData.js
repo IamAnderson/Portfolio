cfcdc6da2cf716a67d693d057c7b6a4e3295f2bd
IamAnderson <omorogbeandersonsimeon@gmail.com>
2022-06-06 11:29:29 +0100

    Initial

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,7 +3,7 @@ export const Work = [
         id:1,
         name:"Resturant Landing Page",
         description:"It is build on top of the React JS, with CSS and also got smooth scrolling attributes.",
-        tags:["react|"," CSS |"," JS"],
+        tags:["react|"," CSS |"," JS|", "#Animation"],
         
         demo:"//caffe-viola.vercel.app/",
         github:"//github.com/IamAnderson/CaffeViola"
@@ -12,7 +12,7 @@ export const Work = [
         id:2,
         name:"Food Recipe Website",
         description:"A food recipe website which fetches data from a  food recipe API.",
-        tags:["react","CSS", "api", "styled-components"],
+        tags:["react|","CSS|", "api|", "styled-components"],
         
         demo:"//food-recipe-five-snowy.vercel.app/",
         github:"//github.com/IamAnderson/Food-recipe"
@@ -22,10 +22,39 @@ export const Work = [
         id:3,
         name:"Sailor",
         description:"A website for a freelance company.",
-        tags:["react", "JS", "styled-components"],
+        tags:["react|", "JS|", "CSS|", "styled-components"],
         
         demo:"//sailor-vert.vercel.app/",
         github:"//github.com/IamAnderson/Sailor"
     },
-  
+
+    {
+        id:4,
+        name:"27",
+        description:"An Ecommerce website template without addition of API, called 27.",
+        tags:["react|", "JS|", "styled-components"],
+        
+        demo:"//27-e-commerce.vercel.app/",
+        github:"//github.com/IamAnderson/27-E-Commerce-"
+    },
+
+    {
+        id:5,
+        name:"GPT-3",
+        description:"A landing page for an AI company.",
+        tags:["react|", "JS|", "styled-components|", "#Animation"],
+        
+        demo:"//gpt-3-inky.vercel.app",
+        github:"//github.com/IamAnderson/GPT3"
+    },
+
+    {
+        id:6,
+        name:"Dolla",
+        description:"A landing page for a financial firm, which utilizes smooth scroll for navigation.",
+        tags:["react|", "JS|", "styled-components|", "react-scroll"],
+        
+        demo:"https://dolla-nine.vercel.app",
+        github:"//github.com/IamAnderson/Dolla"
+    },    
 ] 
