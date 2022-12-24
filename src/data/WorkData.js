f32c81eac01bed76fc0a75892231bbe0f4770f60
Anderson <omorogbeandersonsimeon@gmail.com>
2022-12-24 14:40:22 +0100

    Portfolio Data Update

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -17,8 +17,17 @@ export const Work = [
         demo:"//crypto-analytic.vercel.app/",
         github:"//github.com/IamAnderson/Crypto-Analytic"
     },
+        {
+        id: 3,
+        name:"29",
+        description:"An Ecommerce website called 29. The client side is built on react and redux, while the server side is built with nodejs and express.",
+        tags:["React  |  #Redux","Node | #Express", "Mongodb", "styled-components"],
+        
+        demo:"//29e-commerce.vercel.app/",
+        github:"//github.com/IamAnderson/ECommerce-Store--29/tree/master/frontEnd"
+    },
     {
-        id:3,
+        id: 4,
         name:"Resturant Landing Page",
         description:"It is built on React and styled-components and also got smooth scrolling attributes.",
         tags:["react|"," CSS "," JS", "Animation"],
@@ -27,7 +36,7 @@ export const Work = [
         github:"//github.com/IamAnderson/CaffeViola"
     },
     {
-        id: 4,
+        id: 5,
         name: "Hans Energy",
         description: "A website for an engineering firm. The client side is built with react, while the API is built with node and express.",
         tags: ["React  |  #Redux","Node | #Express", "Mongodb", "styled-components"],
@@ -36,7 +45,7 @@ export const Work = [
         github:"//github.com/IamAnderson/Hans-Energy"
     },
     {
-        id:5,
+        id: 6,
         name:"Food Recipe フードレシピ",
         description:"A food recipe website which fetches data from a food recipe API.",
         tags:["react", "api", "styled-components"],
@@ -44,8 +53,9 @@ export const Work = [
         demo:"//food-recipe-five-snowy.vercel.app/",
         github:"//github.com/IamAnderson/Food-recipe"
     },
+    
     {
-        id:6,
+        id: 7,
         name:"Sailor",
         description:"A website for a freelance company, built with React and styled-components",
         tags:["react", "JS", "CSS", "styled-components"],
@@ -55,17 +65,7 @@ export const Work = [
     },
 
     {
-        id:7,
-        name:"29",
-        description:"An Ecommerce website template without addition of API, called 29.",
-        tags:["react", "JS", "CSS", "styled-components"],
-        
-        demo:"//27-e-commerce.vercel.app/",
-        github:"//github.com/IamAnderson/27-E-Commerce-"
-    },
-
-    {
-        id:8,
+        id: 8,
         name:"GPT-3",
         description:"A landing page for an AI company, built with React and framer motion for animation.",
         tags:["react", "JS", "styled-components", "Animation"],
@@ -75,7 +75,7 @@ export const Work = [
     },
 
     {
-        id:9,
+        id: 9,
         name:"Dolla",
         description:"A landing page for a financial firm, which utilize smooth scroll for navigation.",
         tags:["react", "JS", "styled-components", "react-scroll"],
