2555e2486bf6e4e6977ff7d91436b05b6ef98a6a
IamAnderson <omorogbeandersonsimeon@gmail.com>
2022-12-24 15:05:20 +0100

    Portfolio Data Update

--- src/Components/WorkPage.js
+++ src/Components/WorkPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import { ThemeProvider } from 'styled-components'
 import { darkTheme } from './Themes'
